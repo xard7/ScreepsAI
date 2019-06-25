477c4f2924db9e539b275f25e828662d0dce7bdf
xard7 <lupickiadam@gmail.com>
2019-06-25 20:25:08 +0200

    - prepare to official server

--- creep.engineer.js
+++ creep.engineer.js
@@ -8,27 +8,44 @@ module.exports =
 		}
 		if(creep.memory.plansDone == undefined || !creep.memory.plansDone)
 		{
+			var spawn = Game.spawns["Home"];
+
 			creep.memory.plansDone = true;
 
-			var sources = creep.room.find(FIND_SOURCES);
-			var spawn = Game.spawns["Home"];
-			var paths = [];
-			for(let source of sources)
-			{
-				let p = creep.room.findPath(spawn.pos, source.pos);
-				paths.push(Room.serializePath(p))
-			}
-			{
-				let p = creep.room.findPath(spawn.pos, creep.room.controller.pos);
-				paths.push(Room.serializePath(p));
+			{ // Extensins
+				for(let i = 0; i < 3; i++)
+				{
+
+					creep.room.createConstructionSite(spawn.pos.x - 3 + i, spawn.pos.y + 3, STRUCTURE_EXTENSION);
+					creep.room.createConstructionSite(spawn.pos.x + 3 + i, spawn.pos.y - 3, STRUCTURE_EXTENSION);
+				}
+				for(let i = 0; i < 3; i++)
+				{
+					creep.room.createConstructionSite(spawn.pos.x + 3, spawn.pos.y - 3 + i, STRUCTURE_EXTENSION);
+					creep.room.createConstructionSite(spawn.pos.x - 3, spawn.pos.y + 3 + i, STRUCTURE_EXTENSION);
+				}
 			}
 
-			for(let path of paths)
-			{
-				let p = Room.deserializePath(path)
-				for(let pathVal of p)
+			{ // Roads.
+				var sources = creep.room.find(FIND_SOURCES);
+				var paths = [];
+				for(let source of sources)
 				{
-					let ret = creep.room.createConstructionSite(pathVal.x, pathVal.y, STRUCTURE_ROAD);
+					let p = creep.room.findPath(spawn.pos, source.pos);
+					paths.push(Room.serializePath(p))
+				}
+				{
+					let p = creep.room.findPath(spawn.pos, creep.room.controller.pos);
+					paths.push(Room.serializePath(p));
+				}
+
+				for(let path of paths)
+				{
+					let p = Room.deserializePath(path)
+					for(let pathVal of p)
+					{
+						creep.room.createConstructionSite(pathVal.x, pathVal.y, STRUCTURE_ROAD);
+					}
 				}
 			}
 		}
